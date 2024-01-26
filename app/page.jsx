960393877babc3bdb2ad18d7d701b4b61a08fd99
Lewis Collum <lcollum@ford.com>
2024-01-26 02:58:09 -0500

    added stars

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,19 @@ import { Button, Link, Image } from "@nextui-org/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPinterest } from '@fortawesome/free-brands-svg-icons'
 
+import { useState, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Points, PointMaterial } from '@react-three/drei'
+import * as random from 'maath/random/dist/maath-random.esm'
+
 export default function Home() {
     return (
         <body>
+            <div class="absolute inset-0">
+                <Canvas camera={{ position: [0, 0, 1] }}>
+                    <Stars/>
+                </Canvas>
+            </div>
             <div class="flex text-center justify-center items-center h-full mx-10">
                 <div class="grid grid-rows-1 gap-6 justify-items-center">
                     <div class="font-cursive text-large">twenty</div>
@@ -39,4 +49,20 @@ export default function Home() {
             </div>
         </body>
     )
+}
+
+function Stars(props) {
+  const ref = useRef()
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10
+    ref.current.rotation.y -= delta / 15
+  })
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+        <PointMaterial transparent color="#ffa0e0" size={0.005} sizeAttenuation={true} depthWrite={false} />
+      </Points>
+    </group>
+  )
 }
